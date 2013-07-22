d4063d99fb4f0e49d3bd325ddb9fb7603eb11f0d
Urknecht <florianweiss1@gmx.de>
2013-07-22 23:10:15 +0200

    Chat Server Client

--- server/server.js
+++ server/server.js
@@ -1,43 +1,54 @@
-var app = require('express')()
-    , server = require('http').createServer(app)
-    , io = require('socket.io').listen(server);
+var express = require('express'),
+    sio = require('socket.io'),
+    http = require('http'),
+    app = express();
 
-server.listen(3000);
+var server = http.createServer(app);
+
+app.configure(function () {
+    app.use(express.static(__dirname + '/public'));
+    app.use(app.router);
+});
 
 app.get('/', function (req, res) {
     res.sendfile(__dirname + '/index.html');
 });
 
-app.get('/asd', function (req, res) {
-    res.write("hey!");
-});
+var io = sio.listen(server);
+
+server.listen(8124);
+
+
+var connectedSockets = {} ;
+var connectedCount = 0;
 
 io.sockets.on('connection', function (socket) {
-    socket.on('set nickname', function (name) {
-        socket.set('nickname', name, function () {
-            socket.emit('ready');
-        });
+    socket.on('addme',function(username) {
+        if (connectedSockets[username]) {
+            socket.emit('existing');
+        }
+        else{
+            socket.username = username;
+            connectedCount += 1;
+            connectedSockets[username] = socket;
+            socket.emit('chat', 'SERVER', 'You have connected (currently online:' + connectedCount +')');
+            socket.broadcast.emit('chat', 'SERVER', username + ' is on deck (currently online:' + connectedCount +')');
+        }
     });
 
-    socket.on('msg', function () {
-        socket.get('nickname', function (err, name) {
-            console.log('Chat message by ', name);
-        });
+    socket.on('sendchat', function(data) {
+        io.sockets.emit('chat', socket.username, data);
     });
 
-    socket.emit('news', { hello: 'world' });
-    socket.on('my other event', function (data) {
-        console.log(data);
-    });
-    io.sockets.emit('this', { will: 'be received by everyone'});
-
-    socket.on('private message', function (from, msg) {
-        console.log('I received a private message by ', from, ' saying ', msg);
+    socket.on('whisper', function(username, message) {
+        if(connectedSockets[username]) {
+            connectedSockets[username].emit('whisperMessage', socket.username, message);
+        }
     });
 
-    socket.on('disconnect', function () {
-        io.sockets.emit('user disconnected');
-        console.log('user disconnected');
+    socket.on('disconnect', function() {
+        connectedCount -= 1;
+        connectedSockets[socket.username]=null
+        io.sockets.emit('chat', 'SERVER', socket.username + ' has left the building (currently online:' + connectedCount +')');
     });
-
 });

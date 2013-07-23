9c9dc4226f8060d4788aaddb36dba8821e71c25d
Urknecht <florianweiss1@gmx.de>
2013-07-23 18:57:55 +0200

    Chat Server Client, json test

--- server/server.js
+++ server/server.js
@@ -24,31 +24,63 @@ var connectedCount = 0;
 
 io.sockets.on('connection', function (socket) {
     socket.on('addme',function(username) {
-        if (connectedSockets[username]) {
+        // falls leer oder leerzeichen folgt invalid
+        if(username == null || username.match(/(?:\w+)(?:\s+)(?:\w+)/)){
+            socket.emit('invalid');
+        }
+        else if (connectedSockets[username]) {
             socket.emit('existing');
         }
         else{
             socket.username = username;
             connectedCount += 1;
             connectedSockets[username] = socket;
-            socket.emit('chat', 'SERVER', 'You have connected (currently online:' + connectedCount +')');
+            socket.emit('chat', 'SERVER', 'You have connected');
             socket.broadcast.emit('chat', 'SERVER', username + ' is on deck (currently online:' + connectedCount +')');
+
+            io.sockets.emit('count', connectedCount);
         }
     });
 
     socket.on('sendchat', function(data) {
-        io.sockets.emit('chat', socket.username, data);
+
+        // Testet ob String wie folgt aufgebaut ist: /w name message
+        var reg = /(^|\/\w)(?:\s+)(\w+)(.*)/;
+
+        var match = reg.exec(data);
+
+        /**match[0] beinhaltet ganzen string
+         * match[1] tag
+         * match[2] namen der angewisperten person
+         * match[3] nachricht
+         */
+        //if wisper tag, name und nachricht nicht leer
+        if(match && match[1]=="/w" && match[2] != null && match[3] != null){
+            //falls name vorhanden schicken
+            if(connectedSockets[match[2]]){
+                connectedSockets[match[2]].emit('whisperMessageReceive', socket.username, match[3]);
+                socket.emit('whisperMessageSend', match[2], match [3])
+            }
+            //sonst note schreiben
+            else{
+                socket.emit('chat', 'SERVER', 'User ' + match[2]+' not online');
+            }
+
+        }
+        //normale nachricht
+        else{
+            io.sockets.emit('chat', socket.username, data);
+        }
     });
 
-    socket.on('whisper', function(username, message) {
-        if(connectedSockets[username]) {
-            connectedSockets[username].emit('whisperMessage', socket.username, message);
-        }
+    socket.on('sendJSON', function(data) {
+        //console.log('type:  '+data.type + ' and name:' + data.name);
     });
 
     socket.on('disconnect', function() {
         connectedCount -= 1;
         connectedSockets[socket.username]=null
-        io.sockets.emit('chat', 'SERVER', socket.username + ' has left the building (currently online:' + connectedCount +')');
+        io.sockets.emit('chat', 'SERVER', socket.username + ' has left the building');
+        io.sockets.emit('count', connectedCount);
     });
 });
